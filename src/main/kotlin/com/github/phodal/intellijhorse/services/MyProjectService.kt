026919f750d1cb599bd8ce979bc4b3f51a486f61
GitHub Action <action@github.com>
2023-12-26 05:42:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/phodal/intellijhorse/services/MyProjectService.kt
+++ src/main/kotlin/com/github/phodal/intellijhorse/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.phodal.intellijhorse.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.phodal.intellijhorse.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
